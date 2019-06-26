c8b2165923a2ce461ccb93acdb72b1bdf70fc0bc
William Maddicott <william@Williams-MacBook-Pro.local>
2019-06-26 22:25:04 +0100

    mail.blade changed

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="content-cell" align="center">
+                    @2019 PNR Converter.com
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
